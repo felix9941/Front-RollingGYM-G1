87ae193f37555134fa714f73d3f2f5bce1a3dabd
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-05-23 20:59:25 -0300

    correccion cambios en el nombre de clases de css, agregado de margin top para que el contenido no sea tapado por el navbar y se agrega useEffect para titulo de documento

--- src/pages/SobreNosotros.jsx
+++ src/pages/SobreNosotros.jsx
@@ -1,8 +1,12 @@
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import styles from "../css/SobreNosotros.module.css";
+import { useEffect } from "react";
 
 const SobreNosotros = () => {
+  useEffect(() => {
+    document.title = "Power Gym";
+  }, []);
   return (
     <>
       <div className={styles.imageContainer}>
@@ -15,17 +19,23 @@ const SobreNosotros = () => {
           className={`${styles.imageM} ${styles.backgroundImage}`}
         />
         <div className={styles.textOverlay}>
-          <h1 className="mb-sm-3">¡Bienvenidos a Power Gym!</h1>
-          <h4>
+          <h1 className={`mb-sm-3 mt-5 ${styles.h1SN}`}>
+            ¡Bienvenidos a Power Gym!
+          </h1>
+          <h4 className={styles.h4SN}>
             En Power Gym transformamos el ejercicio en una experiencia única y
             revitalizante. Nuestro objetivo es ayudarte a alcanzar tus metas de
             bienestar físico y mental a través de una combinación perfecta de
             clases energizantes y equipos de última generación.
           </h4>
-          <h1 className={`mt-sm-1 ${styles.fundD}`}>Fundadores</h1>
+          <h1 className={`mt-sm-1 ${styles.fundD} ${styles.h1SN}`}>
+            Fundadores
+          </h1>
         </div>
       </div>
-      <h1 className={`${styles.fundM} text-center`}>Fundadores</h1>
+      <h1 className={`${styles.fundM} ${styles.h1SN} text-center`}>
+        Fundadores
+      </h1>
       <Row
         xs={1}
         sm={2}
@@ -34,10 +44,10 @@ const SobreNosotros = () => {
       >
         <Col key={1}>
           <div className={styles.card}>
-            <h2>Ignacio Duarte</h2>
-            <h3>Co-Fundador y Entrenador Principal</h3>
+            <h2 className={styles.h2SN}>Ignacio Duarte</h2>
+            <h3 className={styles.h3SN}>Co-Fundador y Entrenador Principal</h3>
             <img src="../../public/nachooo.png" alt="Juan Pérez" />
-            <p>
+            <p className={styles.pSN}>
               Con más de 15 años de experiencia en fitness, Nacho es experto en
               entrenamiento personal y nutrición deportiva, comprometido a
               ayudar a los miembros a alcanzar sus metas.
@@ -46,10 +56,12 @@ const SobreNosotros = () => {
         </Col>
         <Col key={2}>
           <div className={styles.card}>
-            <h2>Javier Isasmendi</h2>
-            <h3>Co-Fundador y Especialista en Clases</h3>
+            <h2 className={styles.h2SN}>Javier Isasmendi</h2>
+            <h3 className={styles.h3SN}>
+              Co-Fundador y Especialista en Clases
+            </h3>
             <img src="../../public/nachooo.png" alt="Juan Pérez" />
-            <p>
+            <p className={styles.pSN}>
               Certificado en yoga, pilates y Zumba, aporta su energía y pasión
               por el fitness, haciendo que cada clase sea divertida y accesible
               para todos.
@@ -58,10 +70,12 @@ const SobreNosotros = () => {
         </Col>
         <Col key={3}>
           <div className={styles.card}>
-            <h2>Felix Figueroa</h2>
-            <h3>Co-Fundador y Director de Operaciones</h3>
+            <h2 className={styles.h2SN}>Felix Figueroa</h2>
+            <h3 className={styles.h3SN}>
+              Co-Fundador y Director de Operaciones
+            </h3>
             <img src="../../public/nachooo.png" alt="Juan Pérez" />
-            <p>
+            <p className={styles.pSN}>
               Con experiencia en administración y gestión deportiva, Felix
               asegura una operación perfecta del gimnasio, enfocándose en
               ofrecer la mejor experiencia a nuestros miembros.
