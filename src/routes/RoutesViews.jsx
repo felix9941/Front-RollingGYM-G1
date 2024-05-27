8cb1b9ef2a3a2e47000256b60a4462268fff450e
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-05-27 20:02:55 -0300

    Se crea la pagina de adminsitracion de planes y su ruta

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -5,6 +5,7 @@ import HeroPage from "../pages/HeroPage";
 import SobreNosotros from "../pages/SobreNosotros";
 import IniciarSesion from "../pages/IniciarSesion";
 import Error404 from "../pages/Error404";
+import AdminPlanes from "../pages/AdminPlanes";
 
 const RoutesViews = () => {
   return (
@@ -14,6 +15,7 @@ const RoutesViews = () => {
         <Route path="/iniciarSesion" element={<IniciarSesion />} />
         <Route path="/registro" element={<RegisterPage />} />
         <Route path="/sobreNosotros" element={<SobreNosotros />} />
+        <Route path="/adminPlanes" element={<AdminPlanes />} />
         <Route path="/" element={<HeroPage />} />
         <Route path="*" element={<Error404 />} />
       </Routes>
