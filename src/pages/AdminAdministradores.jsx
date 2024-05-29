7638b0e6acf8fbbc008b4da9c4b78e10d0f0b60d
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-05-29 17:20:27 -0300

    uso de contenedor para no tener que atacar la etiqueta body en el css

--- src/pages/AdminAdministradores.jsx
+++ src/pages/AdminAdministradores.jsx
@@ -3,9 +3,11 @@ import styles from "../css/AdminPages.module.css";
 const AdminAdministradores = () => {
   return (
     <>
-      <div className={styles.encabezadoAdministrador}>
-        <h1 className={styles.h1Admins}>Administracion de Administradores</h1>
-        <button className={styles.buttonAdmins}>Nuevo Administrador</button>
+      <div className={styles.contenedorAdmins}>
+        <div className={styles.encabezadoAdministrador}>
+          <h1 className={styles.h1Admins}>Administracion de Administradores</h1>
+          <button className={styles.buttonAdmins}>Nuevo Administrador</button>
+        </div>
       </div>
     </>
   );
