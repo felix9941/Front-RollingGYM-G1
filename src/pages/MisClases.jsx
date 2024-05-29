87acea5bdb4f2270656687c0707a1f12cdf62fa1
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-05-29 17:40:22 -0300

    se crea un conenedor para no usar la etiqueta body en el css

--- src/pages/MisClases.jsx
+++ src/pages/MisClases.jsx
@@ -3,8 +3,10 @@ import styles from "../css/AdminPages.module.css";
 const MisClases = () => {
   return (
     <>
-      <div className={styles.encabezadoAdministrador}>
-        <h1 className={styles.h1Admins}>Mis Clases</h1>
+      <div className={styles.contenedorAdmins}>
+        <div className={styles.encabezadoAdministrador}>
+          <h1 className={styles.h1Admins}>Mis Clases</h1>
+        </div>
       </div>
     </>
   );
