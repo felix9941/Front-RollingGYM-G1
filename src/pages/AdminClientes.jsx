b44b125ed86860c7d0980e29377044cf36c36bc5
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-05-29 17:23:52 -0300

    contenedor que reemplaza al body

--- src/pages/AdminClientes.jsx
+++ src/pages/AdminClientes.jsx
@@ -3,9 +3,11 @@ import styles from "../css/AdminPages.module.css";
 const AdminClientes = () => {
   return (
     <>
-      <div className={styles.encabezadoAdministrador}>
-        <h1 className={styles.h1Admins}>Administracion de Clientes</h1>
-        <button className={styles.buttonAdmins}>Nuevo Cliente</button>
+      <div className={styles.contenedorAdmins}>
+        <div className={styles.encabezadoAdministrador}>
+          <h1 className={styles.h1Admins}>Administracion de Clientes</h1>
+          <button className={styles.buttonAdmins}>Nuevo Cliente</button>
+        </div>
       </div>
     </>
   );
