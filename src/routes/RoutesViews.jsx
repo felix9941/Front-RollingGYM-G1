745ef07e85dc6f230c8c771ba1a4b6efc25182e5
Felix Figueroa <felixfigueroa41@gmail.com>
2024-05-30 00:43:00 -0300

    Se realiza la pagina principal, las secciones: inicio, reserva, productos, profes y comentarios

--- src/routes/RoutesViews.jsx
+++ src/routes/RoutesViews.jsx
@@ -4,11 +4,13 @@ import RegisterPage from "../pages/Registro";
 import HeroPage from "../pages/HeroPage";
 import SobreNosotros from "../pages/SobreNosotros";
 import IniciarSesion from "../pages/IniciarSesion";
+import HomePage from "../pages/HomePage";
 
 const RoutesViews = () => {
   return (
     <>
       <Routes>
+        <Route path="/principal" element={<HomePage />} />
         <Route path="/contacto" element={<Contacto />} />
         <Route path="/iniciarSesion" element={<IniciarSesion />} />
         <Route path="/registro" element={<RegisterPage />} />
