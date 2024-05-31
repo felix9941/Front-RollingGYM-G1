1d39c3ddebe682b440f66af7f6b484d5d28d0238
Isas321 <javier.isas321@gmail.com>
2024-05-31 00:11:26 -0300

    centrado de boton Reservar + ancho

--- src/pages/ReservarClases.jsx
+++ src/pages/ReservarClases.jsx
@@ -1,90 +1,3 @@
-// import React, { useState } from "react";
-// import Table from "react-bootstrap/Table";
-// import Modal from "react-bootstrap/Modal";
-// import Button from "react-bootstrap/Button";
-// import "../css/ReservarClases.css";
-
-// const ReservarClases = () => {
-//   const [show, setShow] = useState(false);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-
-//   return (
-//     <div className="contenedor-rc">
-//       <div className="contenedor-hijo-rc">
-//         <h1>Reservar Clases</h1>
-//         <Table striped bordered hover>
-//           <thead>
-//             <tr>
-//               <th>Dia</th>
-//               <th>Hora</th>
-//               <th>Profesor</th>
-//               <th>Reservar</th>
-//             </tr>
-//           </thead>
-//           <tbody>
-//             <tr>
-//               <td>Jueves</td>
-//               <td>10:00</td>
-//               <td>Mariano Ocampos</td>
-//               <td>
-//                 <Button variant="primary" onClick={handleShow}>
-//                   Reservar
-//                 </Button>
-//                 <Modal show={show} onHide={handleClose}>
-//                   <Modal.Header closeButton>
-//                     <Modal.Title>Clase de ...</Modal.Title>
-//                   </Modal.Header>
-//                   <Modal.Body>
-//                     Estas seguro de querer reservar esta clase?
-//                   </Modal.Body>
-//                   <Modal.Footer>
-//                     <Button variant="secondary" onClick={handleClose}>
-//                       No
-//                     </Button>
-//                     <Button variant="primary" onClick={handleClose}>
-//                       Si!
-//                     </Button>
-//                   </Modal.Footer>
-//                 </Modal>
-//               </td>
-//             </tr>
-//             <tr>
-//               <td>Jueves</td>
-//               <td>10:00</td>
-//               <td>Mariano Ocampos</td>
-//               <td>
-//                 <Button variant="primary" onClick={handleShow}>
-//                   Reservar
-//                 </Button>
-//                 <Modal show={show} onHide={handleClose}>
-//                   <Modal.Header closeButton>
-//                     <Modal.Title>Clase de ...</Modal.Title>
-//                   </Modal.Header>
-//                   <Modal.Body>
-//                     Estas seguro de querer reservar esta clase?
-//                   </Modal.Body>
-//                   <Modal.Footer>
-//                     <Button variant="secondary" onClick={handleClose}>
-//                       No
-//                     </Button>
-//                     <Button variant="primary" onClick={handleClose}>
-//                       Si!
-//                     </Button>
-//                   </Modal.Footer>
-//                 </Modal>
-//               </td>
-//             </tr>
-//           </tbody>
-//         </Table>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ReservarClases;
-
 import React, { useState } from "react";
 import Table from "react-bootstrap/Table";
 import Modal from "react-bootstrap/Modal";
@@ -101,70 +14,73 @@ const ReservarClases = () => {
     <div className="contenedor-rc">
       <div className="contenedor-hijo-rc">
         <h1>Reservar Clases</h1>
-        <Table bordered hover>
-          <thead className="table-header">
-            <tr>
-              <th>Dia</th>
-              <th>Hora</th>
-              <th>Profesor</th>
-              <th>Reservar</th>
-            </tr>
-          </thead>
-          <tbody className="table-body">
-            <tr>
-              <td>Jueves</td>
-              <td>10:00</td>
-              <td>Mariano Ocampos</td>
-              <td>
-                <Button variant="primary" onClick={handleShow}>
-                  Reservar
-                </Button>
-                <Modal show={show} onHide={handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Clase de ...</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    Estas seguro de querer reservar esta clase?
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      No
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                      Si!
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-              </td>
-            </tr>
-            <tr>
-              <td>Jueves</td>
-              <td>10:00</td>
-              <td>Mariano Ocampos</td>
-              <td>
-                <Button variant="primary" onClick={handleShow}>
-                  Reservar
-                </Button>
-                <Modal show={show} onHide={handleClose}>
-                  <Modal.Header closeButton>
-                    <Modal.Title>Clase de ...</Modal.Title>
-                  </Modal.Header>
-                  <Modal.Body>
-                    Estas seguro de querer reservar esta clase?
-                  </Modal.Body>
-                  <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                      No
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                      Si!
-                    </Button>
-                  </Modal.Footer>
-                </Modal>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
+        <div className="tabla-reserva-clase">
+          {" "}
+          <Table bordered hover>
+            <thead className="table-header">
+              <tr>
+                <th>Dia</th>
+                <th>Hora</th>
+                <th>Profesor</th>
+                <th className="reservar-ancho text-center">Reservar</th>
+              </tr>
+            </thead>
+            <tbody className="table-body">
+              <tr>
+                <td>Jueves</td>
+                <td>10:00</td>
+                <td>Mariano Ocampos</td>
+                <td className="reservar-ancho text-center">
+                  <Button variant="primary" onClick={handleShow}>
+                    Reservar
+                  </Button>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Clase de ...</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      Estas seguro de querer reservar esta clase?
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        No
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Si!
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </td>
+              </tr>
+              <tr>
+                <td>Jueves</td>
+                <td>10:00</td>
+                <td>Mariano Ocampos</td>
+                <td className="reservar-ancho text-center">
+                  <Button variant="primary" onClick={handleShow}>
+                    Reservar
+                  </Button>
+                  <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                      <Modal.Title>Clase de ...</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                      Estas seguro de querer reservar esta clase?
+                    </Modal.Body>
+                    <Modal.Footer>
+                      <Button variant="secondary" onClick={handleClose}>
+                        No
+                      </Button>
+                      <Button variant="primary" onClick={handleClose}>
+                        Si!
+                      </Button>
+                    </Modal.Footer>
+                  </Modal>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+        </div>
       </div>
     </div>
   );
