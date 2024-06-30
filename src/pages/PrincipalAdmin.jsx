199b5fb3a273eddc34b9af8b20b8ffcf616fb9aa
Ignaci0-Duarte <118497938+Ignaci0-Duarte@users.noreply.github.com>
2024-06-30 15:59:11 -0300

    Se agrega la logica para que al pasar el dia de la clase las reservas se limpien

--- src/pages/PrincipalAdmin.jsx
+++ src/pages/PrincipalAdmin.jsx
@@ -1,15 +1,107 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import styles from "../css/AdminPrincipal.module.css";
+import clienteAxios from "../helpers/clienteAxios";
 
 const PrincipalAdmin = () => {
   const navigate = useNavigate();
 
+  const [dia, setDia] = useState("");
+  const [clases, setClases] = useState([]);
+  const [clientes, setClientes] = useState([]);
+  const [body, setBody] = useState({});
+
+  const today = new Date();
+  const daysOfWeek = [
+    "Domingo",
+    "Lunes",
+    "Martes",
+    "Miércoles",
+    "Jueves",
+    "Viernes",
+    "Sábado",
+  ];
+
   useEffect(() => {
     document.title = "Principal Administrador";
+    getDayOfWeek(today);
+    getClientes();
   }, []);
+
+  useEffect(() => {
+    if (dia !== "") {
+      mostrarDia();
+    }
+  }, [dia]);
+
+  const getClientes = async () => {
+    try {
+      const response = await clienteAxios.get("/clientes/");
+      setClientes(response.data.clientes);
+    } catch (error) {
+      console.error("Error al obtener los clientes", error);
+    }
+  };
+
+  const getDayOfWeek = (date) => {
+    let dayIndex = new Date(date).getDay();
+    if (dayIndex === 0) {
+      dayIndex = 7;
+    }
+    setDia(dayIndex - 1);
+  };
+
+  const mostrarDia = async () => {
+    try {
+      const nuevasClases = [];
+      for (let index = dia; index > 0; index--) {
+        const response = await clienteAxios.get(
+          `/clases/dia/${daysOfWeek[index]}`
+        );
+        nuevasClases.push(...response.data.clasesDia);
+      }
+      setClases(nuevasClases);
+    } catch (error) {
+      console.error("Error al obtener las clases del día", error);
+    }
+  };
+
+  useEffect(() => {
+    if (clases.length > 0) {
+      reservasCero();
+      limpiarReservas();
+    }
+  }, [clases]);
+
+  const reservasCero = async () => {
+    try {
+      const promesas = clases.map((clase) =>
+        clienteAxios.put(`/clases/cero/${clase._id}`, {})
+      );
+      const respuestas = await Promise.all(promesas);
+    } catch (error) {
+      console.error("Error al actualizar las clases", error);
+    }
+  };
+
+  const limpiarReservas = async () => {
+    try {
+      const promesas = clientes.flatMap((cliente) =>
+        clases.map((clase) => {
+          const body = {
+            reservas: cliente.idReservas,
+            idClase: clase._id,
+          };
+          return clienteAxios.delete("/reservas/limpieza", { data: body });
+        })
+      );
+      const respuestas = await Promise.all(promesas);
+    } catch (error) {
+      console.error("Error al limpiar reservas", error);
+    }
+  };
 
   const pages = [
     { name: "Home Page PowerGYM", route: "/principal" },
